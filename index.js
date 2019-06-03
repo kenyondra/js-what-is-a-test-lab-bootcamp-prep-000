383b259c1eb6e0b1464c7f9f306d86679df1b95b
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-03 16:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 34
-var message = ` is ${height} inches tall`
+var message = ` is ${34} inches tall`
 
 module.exports = {name, height, message}
